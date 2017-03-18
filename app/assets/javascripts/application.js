30dffa6edb2eeaf7fe67f624d0499822dabd1e92
tlockAOB <tlock@armyofbees.com>
2017-03-18 21:26:09 +0700

    stream page initial sketch and layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,10 @@
 //= require turbolinks
 //= require tether
 //= require bootstrap-sprockets
+
+
+$(document).on('turbolinks:load', function(){
+  $('.panel-toggle button').on('click', function(){
+    $(this).closest($('.panel')).toggleClass('open closed');
+  });
+});
