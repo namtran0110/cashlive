3082753c0e58074e248283012f9bb42e74cbafaa
tlockAOB <tlock@armyofbees.com>
2017-03-18 17:47:20 +0700

    add sketches controller, view directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # DEVELOPMENT ONLY: sketches are html/css/js layouts with no logic yet,
+  # the code can be reused elsewhere when backend ready
+  get 'sketches/stream' => 'sketches#stream'
 end
