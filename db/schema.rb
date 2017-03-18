2eee2a1eb1ab618b37e8bca3c1b6c6c76485be38
tlockAOB <tlock@armyofbees.com>
2017-03-19 02:17:05 +0700

    migration ran for avatar

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 20170318235803) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "avatar_path"
     t.string   "username"
     t.string   "phone_number"
     t.text     "description"
