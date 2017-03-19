e194267138f4a362f8ac0b5ae00bd303df06103e
tlockAOB <tlock@armyofbees.com>
2017-03-19 23:35:11 +0700

    add username to devise fields, and use callback to update store slug if username changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,19 @@ class User < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
+
+  # Actions for Store on user creation / update
   after_create :generate_empty_store
+  after_update :update_store_slug
 
   private
 
     def generate_empty_store
       Store.create!(user_id: self.id, slug: self.username.parameterize)
+    end
+
+    def update_store_slug
+      self.store.update(slug: self.username.parameterize)
     end
 
 end
