4f3da21793d8c3ea5b641dc7946cb50bdf44a4a1
tlockAOB <tlock@armyofbees.com>
2017-03-21 21:19:08 +0700

    render 404 when no store exists at given route

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,8 @@
 class StoresController < ApplicationController
   def show
     @store = Store.find_by_slug(params[:id])
+    if @store == nil
+      render file: "#{Rails.root}/public/404.html", layout: false
+    end
   end
 end
