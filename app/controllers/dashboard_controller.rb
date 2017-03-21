dc553b094c573a538daed8a31ae859beabe17c8f
tlockAOB <tlock@armyofbees.com>
2017-03-21 21:16:39 +0700

    fix flash message bootstrap class: 'fade in' --> 'fade show'

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,10 +3,9 @@ class DashboardController < ApplicationController
     unless current_user
       flash[:error] = "You must login to view Dashboard"
       redirect_to user_session_path
+    else
+      @user = current_user
+      @stores = Store.where.not(user_id: @user.id)
     end
-
-    @user = current_user
-    @stores = Store.where.not(user_id: @user.id)
-
   end
 end
