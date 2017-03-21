21d3989e2454a8f8b3f01b7de1105e86a6af855d
tlockAOB <tlock@armyofbees.com>
2017-03-21 21:36:54 +0700

    add login, logout, and register links to navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_id(params[:id])
-    if @user == nil 
+    if @user == nil
       render file: "#{Rails.root}/public/404.html", layout: false
     end
   end
