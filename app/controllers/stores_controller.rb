f8ff855785beb01adcfd96fb0ee5718127205f2f
tlockAOB <tlock@armyofbees.com>
2017-03-23 00:27:16 +0700

    initialize friendly id, and configure stores properly

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,6 +1,6 @@
 class StoresController < ApplicationController
   def show
-    @store = Store.find(params[:id])
+    @store = Store.friendly.find(params[:id])
     if @store == nil
       render file: "#{Rails.root}/public/404.html", layout: false
     end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,7 +1,10 @@
 class Store < ApplicationRecord
-  belongs_to :user
 
+  belongs_to :user
   has_many :messages, dependent: :destroy
+
+  extend FriendlyId
+  friendly_id :name
 
   validates :name, presence: true, uniqueness: { message: "Username already in use" }
 end
