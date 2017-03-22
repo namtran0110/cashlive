699f7a3d0297b81bb4962d650e0cd03d59e50df7
tlockAOB <tlock@armyofbees.com>
2017-03-22 22:57:26 +0700

    add migration to remove slug column from stores (friendly id makes it redundant) and run it (modify schema)

---
+++ db/migrate/20170322155444_remove_slug_from_stores.rb
@@ -0,0 +1,5 @@
+class RemoveSlugFromStores < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :stores, :slug
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319184235) do
+ActiveRecord::Schema.define(version: 20170322155444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20170319184235) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "slug"
     t.index ["user_id"], name: "index_stores_on_user_id", using: :btree
   end
 
