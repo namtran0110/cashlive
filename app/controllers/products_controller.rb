ee4f6743766a5b159c74b52dcd0bb5b867f2407c
Nam Tran <namtran0110@gmail.com>
2017-03-23 15:39:31 +0700

    in process

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,4 @@
 class ProductsController < ApplicationController
-  def new
-    @product = Product.new
-  end
-
-  def create
-    @product = Product.new product_params
-    if @product.save
-      flash[:success] = "You just created #{@product.name}"
-      session[:product_id] = @product.id
-      redirect_to root_path
-    else
-      render 'new'
-    end
-  end
 
   def index
     @products = Product.all
@@ -22,12 +8,27 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new product_params
+    if @product.save
+      flash[:success] = "You just created #{@product.name}"
+      session[:product_id] = @product.id
+      redirect_to store_path(@store)
+    else
+      render 'new'
+    end
+  end
+
   def edit
   end
 
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, :description)
+    params.require(:product).permit(:name, :price, :description, :avatar)
   end
 end
