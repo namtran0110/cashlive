33255460c8c111be6725dde046aa8e1b7a7b0cec
Ken <votantho93@gmail.com>
2017-03-24 21:06:39 +0700

    subscible user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
-  root "home#index"
+  get ':username/follow_user', to: 'relationships#follow_user', as: :follow_user
+  get ':username/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
 
+  root "home#index"
   get '/dashboard' => 'dashboard#show', as: :dashboard
 
   devise_for :users
-  resources :users, only: [:show, :create]
+  resources :users, only: [:create]
+  get ':username' =>  'users#show'
   resources :stores, only: [:show, :edit, :update]
 
   # DEVELOPMENT ONLY: sketches are html/css/js layouts with no logic yet,
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319124226) do
+ActiveRecord::Schema.define(version: 20170321193830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.integer  "following_id", null: false
+    t.integer  "follower_id",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["follower_id"], name: "index_follows_on_follower_id", using: :btree
+    t.index ["following_id", "follower_id"], name: "index_follows_on_following_id_and_follower_id", unique: true, using: :btree
+    t.index ["following_id"], name: "index_follows_on_following_id", using: :btree
+  end
 
   create_table "stores", force: :cascade do |t|
     t.integer  "user_id"
