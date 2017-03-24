ae8972ae9f305cc8f967539dc44491ff8ed22ef9
Ken <votantho93@gmail.com>
2017-03-24 21:06:39 +0700

    subscible user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323072142) do
+ActiveRecord::Schema.define(version: 20170322081341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,16 @@ ActiveRecord::Schema.define(version: 20170323072142) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.index ["store_id"], name: "index_products_on_store_id", using: :btree
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer  "following_id", null: false
+    t.integer  "follower_id",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["follower_id"], name: "index_follows_on_follower_id", using: :btree
+    t.index ["following_id", "follower_id"], name: "index_follows_on_following_id_and_follower_id", unique: true, using: :btree
+    t.index ["following_id"], name: "index_follows_on_following_id", using: :btree
   end
 
   create_table "stores", force: :cascade do |t|
