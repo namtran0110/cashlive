21d8a158a737114f98bec5a1b64027f56e6232bb
Ken <votantho93@gmail.com>
2017-03-24 21:06:39 +0700

    subscible user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,30 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 
+  # Config paperclip
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
+  # Config subscribe
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following, through: :following_relationships, source: :following
 
   # Actions
   after_create :generate_empty_store
   after_update :update_store_slug
+
+  def follow(user_id)
+    following_relationships.create(following_id: user_id)
+  end
+
+  def unfollow(user_id)
+    following_relationships.find_by(following_id: user_id).destroy
+  end
 
   private
 
@@ -25,5 +40,4 @@ class User < ApplicationRecord
     def update_store_slug
       self.store.update(name: self.username)
     end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170322081341) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +45,16 @@ ActiveRecord::Schema.define(version: 20170322081341) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["store_id"], name: "index_products_on_store_id", using: :btree
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer  "following_id", null: false
+    t.integer  "follower_id",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["follower_id"], name: "index_follows_on_follower_id", using: :btree
+    t.index ["following_id", "follower_id"], name: "index_follows_on_following_id_and_follower_id", unique: true, using: :btree
+    t.index ["following_id"], name: "index_follows_on_following_id", using: :btree
   end
 
   create_table "stores", force: :cascade do |t|
