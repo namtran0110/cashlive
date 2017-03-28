a4a32d310d0248d7433273dc9b8ea94aee41cfdb
Ken <votantho93@gmail.com>
2017-03-29 04:08:56 +0700

    :ghost: fix some fuking bugs :ghost:

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,19 @@ class User < ApplicationRecord
     following_relationships.find_by(following_id: user_id).destroy
   end
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+
+
   private
 
     def generate_empty_store
@@ -41,7 +54,7 @@ class User < ApplicationRecord
       StreamInstance.create!(store_id: store.id)
     end
 
-    def update_store_slug
-      self.store.update(name: self.username)
-    end
+  def update_store_slug
+    self.store.update(name: self.username)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     resources :products
   end
 
-  devise_for :users, controllers: { sessions: 'users/sessions', omniauth_callbacks: "user/omniauth_callbacks" }
+  devise_for :users, controllers: { sessions: 'users/sessions', omniauth_callbacks: "users/omniauth_callbacks" }
 
   resources :users, only: [:create]
   get ':username' =>  'users#show'
