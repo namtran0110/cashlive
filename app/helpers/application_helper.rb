ad329175718b00c54b83b8a2f6136258c05e12c2
tlockAOB <tlock@armyofbees.com>
2017-04-02 09:59:44 +0700

    change sign in/out flash messages to display as info not warning

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def bootstrap_class_for flash_type
-    { success: 'alert-success', error: 'alert-danger', notice: 'alert-warning'}[flash_type.to_sym]
+    { success: 'alert-success', error: 'alert-danger', notice: 'alert-info'}[flash_type.to_sym]
   end
 
   def flash_messages(opts = {})
