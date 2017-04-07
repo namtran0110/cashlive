8eab82aedab68f47719559870117381e89bb2251
Nam Tran <namtran0110@gmail.com>
2017-04-07 11:50:38 +0700

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,32 @@
 puts "seeding data"
-sleep 1
 
 # Begin Demo-Ready Seed Data
 ##
 ## USE THIS FORMULA TO ADD YOUR SEEDS. Put images in "seed_images" folder first.
 ##
 puts "generating users..."
-user01 = User.new password: "secret", email: "armyofbees@example.com", username: "ArmyOfBees", avatar: File.new('seed_images/armyofbees.png')
-user02 = User.new password: "secret", email: "tlock@example.com", username: "TLock", avatar: File.new('seed_images/tlock.jpg')
-user03 = User.new password: "secret", email: "superstore@example.com", username: "SuperStore", avatar: File.new('seed_images/superstore.jpg')
+user01 = User.new password: "123123", email: "armyofbees@example.com", username: "ArmyOfBees", avatar: File.new('seed_images/armyofbees.png')
+user02 = User.new password: "123123", email: "tlock@example.com", username: "TLock", avatar: File.new('seed_images/tlock.jpg')
+user03 = User.new password: "123123", email: "wholefood@example.com", username: "Whole Foods", avatar: File.new('seed_images/whole_foods.jpg')
+user04 = User.new password: "123123", email: "cena@gmail.com", username: "John Cena", avatar:File.new('seed_images/cena.jpg')
+user05 = User.new password: "123123", email: "namtran0110@gmail.com", username: "Nam Tran", avatar:File.new('seed_images/founder.jpg')
+user06 = User.new password: "123123", email: "hutieu@gmail.com", username: "Anh Ba", avatar:File.new('seed_images/anh_ba.jpg')
+user07 = User.new password: "123123", email: "harley@gmail.com", username: "Harley Trung", avatar:File.new('seed_images/harley.jpg')
+user08 = User.new password: "123123", email: "chang@gmail.com", username: "Changmakeup", avatar:File.new('seed_images/changmakeup.jpg')
+user09 = User.new password: "123123", email: "steve@gmail.com", username: "Steve Jobs", avatar:File.new('seed_images/steve.jpg')
+user10 = User.new password: "123123", email: "ricknmorty@gmail.com", username: "Rick & Morty", avatar:File.new('seed_images/rick.jpg')
 
 puts "saving users to database..."
 user01.save
 user02.save
 user03.save
+user04.save
+user05.save
+user06.save
+user07.save
+user08.save
+user09.save
+user10.save
 Store.update_all(description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde totam, tempora perferendis esse natus animi fuga sit voluptatibus inventore odit nihil cum quae ad provident debitis praesentium aspernatur laboriosam nulla eos assumenda iste, voluptatem obcaecati ipsa. Mollitia assumenda tempora earum deleniti quibusdam. Maxime tempora debitis est porro? Non, nobis, incidunt.")
 
 puts "linking subscriptions..."
@@ -26,23 +39,53 @@ Follow.create following_id: user01.id, follower_id: user03.id
 puts "adding store cover photos..."
 Store.find(user01.store.id).update(cover_photo: File.new('seed_images/hive.jpg'))
 Store.find(user02.store.id).update(cover_photo: File.new('seed_images/tlock_cover.jpg'))
-Store.find(user03.store.id).update(cover_photo: File.new('seed_images/super_cover.jpg'))
-
+Store.find(user03.store.id).update(cover_photo: File.new('seed_images/whole_foods_cover.jpg'))
+Store.find(user04.store.id).update(cover_photo: File.new('seed_images/cena_background.jpg'))
+Store.find(user05.store.id).update(cover_photo: File.new('seed_images/nam_cover.jpg'))
+Store.find(user06.store.id).update(cover_photo: File.new('seed_images/ban_hu_tieu.jpg'))
+Store.find(user07.store.id).update(cover_photo: File.new('seed_images/trung_cover.jpg'))
+Store.find(user08.store.id).update(cover_photo: File.new('seed_images/changmakeup_cover.jpg'))
+Store.find(user09.store.id).update(cover_photo: File.new('seed_images/apple.jpg'))
+Store.find(user10.store.id).update(cover_photo: File.new('seed_images/ricknmorty.jpg'))
 puts "generating products..."
-Product.create store_id: user01.store.id, name: "Guitar", price: 1000000, image: File.new('seed_images/guitar.jpg'),  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde laboriosam dignissimos placeat, voluptate temporibus omnis recusandae nam autem saepe officiis!"
-Product.create store_id: user01.store.id, name: "Keyboard", price: 1200000, image: File.new('seed_images/keyboard.jpg'),  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, excepturi!"
-Product.create store_id: user01.store.id, name: "Ukelele", price: 900000, image: File.new('seed_images/ukelele.jpg'),  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum asperiores nemo veritatis at, eos iure."
+Product.create store_id: user01.store.id, name: "Guitar", price: 1000000, image: File.new('seed_images/guitar.jpg'),  description: "It's a Guitar"
+Product.create store_id: user01.store.id, name: "Keyboard", price: 1200000, image: File.new('seed_images/keyboard.jpg'),  description: "The Less Expensive Piano"
+Product.create store_id: user01.store.id, name: "Ukelele", price: 900000, image: File.new('seed_images/ukelele.jpg'),  description: "I'm yours"
 
-Product.create store_id: user02.store.id, name: "Honda Wave RSX", price: 24490000, image: File.new('seed_images/wave.png'),  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde laboriosam dignissimos placeat, voluptate temporibus omnis recusandae nam autem saepe officiis!"
-Product.create store_id: user02.store.id, name: "Full Face Helmet", price: 400000, image: File.new('seed_images/fullface.jpg'),  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, excepturi!"
-Product.create store_id: user02.store.id, name: "3/4 Helmet", price: 200000, image: File.new('seed_images/threequarter.jpg'),  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum asperiores nemo veritatis at, eos iure."
+Product.create store_id: user02.store.id, name: "Honda Wave RSX", price: 24490000, image: File.new('seed_images/wave.png'),  description: "The best wave you can ride"
+Product.create store_id: user02.store.id, name: "Full Face Helmet", price: 400000, image: File.new('seed_images/fullface.jpg'),  description: "Play Safe"
+Product.create store_id: user02.store.id, name: "3/4 Helmet", price: 200000, image: File.new('seed_images/threequarter.jpg'),  description: "Protection For Your Head"
 
-Product.create store_id: user03.store.id, name: "Xoai", price: 15000, image: File.new('seed_images/xoai.jpg'),  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde laboriosam dignissimos placeat, voluptate temporibus omnis recusandae nam autem saepe officiis!"
-Product.create store_id: user03.store.id, name: "Dau", price: 10000, image: File.new('seed_images/dau.jpg'),  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, excepturi!"
-Product.create store_id: user03.store.id, name: "Viet Quat", price: 10000, image: File.new('seed_images/vietquat.jpg'),  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum asperiores nemo veritatis at, eos iure."
-Product.create store_id: user03.store.id, name: "Dua", price: 20000, image: File.new('seed_images/dua.jpg'),  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum asperiores nemo veritatis at, eos iure."
+Product.create store_id: user03.store.id, name: "Xoai", price: 15000, image: File.new('seed_images/xoai.jpg'),  description: "Mango Loco"
+Product.create store_id: user03.store.id, name: "Dau", price: 10000, image: File.new('seed_images/dau.jpg'),  description: "Strawberry! Honey"
+Product.create store_id: user03.store.id, name: "Viet Quat", price: 10000, image: File.new('seed_images/vietquat.jpg'),  description: "Raspberry Baby!"
+Product.create store_id: user03.store.id, name: "Dua", price: 20000, image: File.new('seed_images/dua.jpg'),  description: "Coco nutz"
 
-sleep 1
+Product.create store_id: user04.store.id, name: "WWE Belt", price:120000000, image: File.new('seed_images/wwe.jpg'), description: "The only true champion with the belt"
+Product.create store_id: user04.store.id, name: "John Cena House", price:770000000, image: File.new('seed_images/cena_house.jpg'), description: "The Beautiful Condo Near The Beach"
+Product.create store_id: user04.store.id, name: "U Can't See Me", price: 60000, image: File.new('seed_images/cena_hat.jpg'), description: "U Can't See Me"
+
+Product.create store_id: user05.store.id, name: "Vespa", price:59000000, image: File.new('seed_images/vespa.jpg'), description: "Cool Bike"
+Product.create store_id: user05.store.id, name: "Audi A5", price:755000000, image: File.new('seed_images/2016-audi-a5.jpg'), description: "The Best Car In The World"
+Product.create store_id: user05.store.id, name: "Tesla Model X", price: 949000000, image: File.new('seed_images/2016-Tesla-Model-X-P90D.jpg'), description: "You Don't Need To Drive Anymore"
+
+Product.create store_id: user06.store.id, name: "Hu tieu Nam Vang", price: 40000, image: File.new('seed_images/nam_vang.jpg'), description: "Hu tieu re ngon"
+Product.create store_id: user06.store.id, name: "Hu tieu xuong", price: 40000, image: File.new('seed_images/hu_tieu_xuong.jpg'), description: "Hu tieu re ngon"
+Product.create store_id: user06.store.id, name: "Hu tieu kho", price: 40000, image: File.new('seed_images/hu_tieu_kho.jpg'), description: "Hu tieu re ngon"
+
+Product.create store_id: user07.store.id, name: "Rails videos", price: 1400000, image: File.new('seed_images/rails.jpg'), description: "The best tutorial course you can get"
+Product.create store_id: user07.store.id, name: "Swift iOS videos", price: 1100000, image: File.new('seed_images/swift.jpg'), description: "The best tutorial course you can get"
+
+Product.create store_id: user08.store.id, name: "3CE Mood Recipe Matte Lip Color", price: 290000, image: File.new('seed_images/3ce.jpg'), description: "Beauty Product"
+Product.create store_id: user08.store.id, name: "Shiseido Rouge Rouge Lipsticks", price: 270000, image: File.new('seed_images/shiseido.jpg'), description: "Beauty Product"
+
+Product.create store_id: user09.store.id, name: "Macbook Pro Retina", price: 27900000, image: File.new('seed_images/macbook.jpg'), description: "Designed by the best, Produced by the top, Created for the common"
+Product.create store_id: user09.store.id, name: "iPhone 5", price: 1870000, image: File.new('seed_images/iphone.jpg'), description: "Designed by the best, Produced by the top, Created for the common"
+Product.create store_id: user09.store.id, name: "iPod", price: 970000, image: File.new('seed_images/ipod.jpg'), description: "Designed by the best, Produced by the top, Created for the common"
+
+Product.create store_id: user10.store.id, name: "Spaceship", price: 122000000, image: File.new('seed_images/spaceship.jpg'), description: "Come with inception battery"
+Product.create store_id: user10.store.id, name: "Plumbus", price: 810000, image: File.new('seed_images/plumbus.jpg'), description: "I don't even know wtf this is"
+Product.create store_id: user10.store.id, name: "Meeseeks", price: 1000000, image: File.new('seed_images/meeseeks.jpg'), description: "Do what you order till I die"
 
 puts "generating Order statuses..."
 OrderStatus.delete_all
